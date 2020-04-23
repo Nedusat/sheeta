9699433ec4fa04c3bff1b558940e0dc01a53bf47
Edvin Vikström <spelfanta2@gmail.com>
2020-04-23 21:27:34 +0000

    audio drivers

---
+++ kernel/definitions.h
@@ -0,0 +1,9 @@
+#ifndef DEFINITIONS
+  #define DEFINITIONS
+
+typedef unsigned char         uint8_t
+typedef unsigned short        uint16_t
+typedef unsigned int          uint32_t
+typedef unsigned long         uint64_t
+
+#endif
--- kernel/drivers/audio/mad.h
+++ kernel/drivers/audio/mad.h
@@ -3,4 +3,15 @@
 #ifndef MAD_H
   #define MAD_H
 
+#include "../../definitions.h"
+
+struct MAD_audio_device {
+  const char* identifier;
+};
+
+struct MAD_audio_buffer {
+  uint32_t length;
+  uint8_t* buffer;
+};
+
 #endif
