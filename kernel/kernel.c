10c4e053060cfe55f8d557eef46a1d14ad673dc2
Edvin Vikström <spelfanta2@gmail.com>
2020-04-23 21:08:26 +0000

    kernel_init()

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,5 @@
 #include "kernel.h"
 
-void kernel_init();
-
 void kernel_init()
 {
 	
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,9 +1,11 @@
 #ifndef KERNEL_H
-#define KERNEL_H
+  #define KERNEL_H
 
 #include "general.h"
 
 u16* VGA_BUFFER;
 u16 VGA_INDEX;
+
+int kernel_init();
 
 #endif
